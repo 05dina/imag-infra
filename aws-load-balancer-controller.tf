79db46f2c785896a76e7e690ddd5f0dd60e76aac
EDH <05.dina_enfermeras@icloud.com>
2025-03-28 16:56:57 +0000

    💚Avoid error by cluster  absence

--- aws-load-balancer-controller.tf
+++ aws-load-balancer-controller.tf
@@ -11,17 +11,17 @@ data "aws_eks_cluster_auth" "this" {
 
 provider "kubernetes" {
   alias                  = "eks"
-  host                   = data.aws_eks_cluster.this.endpoint
-  token                  = data.aws_eks_cluster_auth.this.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  host                   = try(data.aws_eks_cluster.this.endpoint, "")
+  token                  = try(data.aws_eks_cluster_auth.this.token, "")
+  cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.this.certificate_authority[0].data), "")
 }
 
 provider "helm" {
   alias = "eks"
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    token                  = data.aws_eks_cluster_auth.this.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    host                   = try(data.aws_eks_cluster.this.endpoint, "")
+    token                  = try(data.aws_eks_cluster_auth.this.token, "")
+    cluster_ca_certificate = try(base64decode(data.aws_eks_cluster.this.certificate_authority[0].data), "")
   }
 }
 
